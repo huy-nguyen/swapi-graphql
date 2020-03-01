58a6e270fe7e5a8331df22379d1dd911d1df4149
Huy Nguyen <me@huy.dev>
2020-02-29 22:24:15 -0500

    Build with rollup instead of babel
    Build with rollup allows us to bundle all dependencies into a single output
    file instead of creating many loose files.

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/index',
+  output: {
+    file: 'lib/index.js',
+    format: 'cjs',
+  },
+  plugins: [
+    json(),
+    resolve(),
+    commonjs({
+      exclude: 'src/**',
+      include: [/node_modules/],
+      namedExports: {
+        dataloader: ['default'],
+        'graphql-relay': [
+          'nodeDefinitions',
+          'fromGlobalId',
+          'connectionDefinitions',
+          'connectionArgs',
+          'connectionFromArray',
+        ],
+      },
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
